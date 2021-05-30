a2d24457974ec07034352e86b60d1a29b7424313
Samuel Antwi <samuelantwidev@gmail.com>
2021-05-30 09:18:49 +0100

    Add Footer component

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,8 @@
+//A  Layout component wrapper that provides navigation and footer to all the pages
+
 import Navbar from './Navbar'
 import Head from 'next/head'
+import Footer from './Footer'
 
 const Layout = ({ children, title, description }) => {
   return (
@@ -12,12 +15,15 @@ const Layout = ({ children, title, description }) => {
         <meta name='description' content={description} />
       </Head>
       <div className='pt-10 md:pt-40'>{children}</div>
+      <Footer />
     </div>
   )
 }
 
 export default Layout
 
+// Default title and description
+// This gets overwritten if custom title and desscription are provided in individual pages.
 Layout.defaultProps = {
   title: 'Frontend developer interview test for Samuel antwi',
   description:
